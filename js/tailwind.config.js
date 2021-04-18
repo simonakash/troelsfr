048a11a5b6a2c4727961b1003a914102eadf8362
SAYMON AREFIN AKASH <75067011+simonakash@users.noreply.github.com>
2021-04-19 05:39:32 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+    // ...
+    variants: {
+      extend: {
+        textColor: ['visited'],
+      }
+    },
+}
+
+// tailwind.config.js
+module.exports = {
+    // ...
+    variants: {
+      extend: {
+        backgroundColor: ['active'],
+      }
+    },
+}
